8f157f053db60f9a27666f956443dbdebb55881c
Harrison-Naranjo <harrisonsteven921120@gmail.com>
2025-02-03 10:46:08 -0500

    feat: estilos para login feat: tailwind config feeat: proyecto corriendo

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {BrowserRouter, Route, Routes, Navigate} from 'react-router';
+import LoginLayout from './features/auth/login/login-layout/login-layout.tsx';
+import { ThemeProvider } from './components/heme-provider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<Navigate to="/login" replace={true} />} />
+                  <Route path="/home" element={<App />} />
+                  <Route path="/login" element={<LoginLayout />} />
+              </Routes>
+          </BrowserRouter>
+      </ThemeProvider>
+  </StrictMode>
 )
+
+
